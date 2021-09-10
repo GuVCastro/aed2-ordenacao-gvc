03c51ebc764147734b4e5234b71728ff01b0dd4a
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-09-10 20:55:07 -0300

    Initial commit

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+/* Project libraries */
 #include "bolha.h"
 #include "countingsort.h"
 #include "heapsort.h"
@@ -6,81 +7,64 @@
 #include "quicksort.h"
 #include "selecao.h"
 #include "shellsort.h"
-
+/* Standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdlib.h>
-//=============================================================================
-void teste(int n, char ordem){
+void teste(int n, char ordem)
+{
+	//Delcaracao de variaveis
+	//int n = (argc < 2) ? 10000 : atoi(argv[1]);
+	int *array = (int *) malloc(n * sizeof(int));
 
-   //Delcaracao de variaveis
-    //int n = (argc < 2) ? 10000 : atoi(argv[1]);
-    int *array = (int*) malloc(n*sizeof(int));
-    
-    // VARIAVEIS PARA CALCULAR O TEMPO
-    clock_t inicio, fim;
-    double total;
+	// VARIAVEIS PARA CALCULAR O TEMPO
+	clock_t inicio, fim;
+	double total;
 
+	//Geracao do conjunto a ser ordenado
+	switch(ordem){
+		case 'C': crescente(array, n);
+		break;
+		case 'D': decrescente(array, n);
+		break;
+		case 'A': aleatorio(array, n);
+		break;
+		default: aleatorio(array, n);
+	}
 
-    //Geracao do conjunto a ser ordenado
-    switch(ordem){
-      case 'C': crescente(array, n);
-      break;
-      case 'D': decrescente(array, n);
-      break;
-      case 'A':  aleatorio(array, n);
-      break;
-      default: aleatorio(array, n);
-    }
-	 
-    //Mostrar o conjunto a ser ordenado
-	 //mostrar(array, n);
-		
+	//Mostrar o conjunto a ser ordenado
+	//mostrar(array, n);
 
-    //Execucao do algoritmo de ordenacao
-	 inicio = clock();
-    //bolha(array, n);
-    //countingsort(array, n);
-   // heapsort(array, n);
-    insercao(array, n);
-    //mergesort(array, n);
-    //quicksort(array, n);
-    //selecao(array, n);
-    //shellsort(array, n);
-	 fim = clock();
-  total = ((fim - inicio) / (double)CLOCKS_PER_SEC);    
+	//Execucao do algoritmo de ordenacao
+	inicio = clock();
+	//bolha(array, n);
+	//countingsort(array, n);
+	// heapsort(array, n);
+	insercao(array, n);
+	//mergesort(array, n);
+	//quicksort(array, n);
+	//selecao(array, n);
+	//shellsort(array, n);
+	fim = clock();
+	total = ((fim - inicio) / (double)CLOCKS_PER_SEC);    
 
+	//Mostrar o conjunto ordenado, tempo de execucao e status da ordenacao
+	//algoritmo.mostrar(array, n);
+	printf("N: %d - T: - %f s - O: %c.\n",n, total, ordem);
+	//printf("isOrdenado: %s\n", isOrdenado(array, n) ? "true" : "false");
 
-    //Mostrar o conjunto ordenado, tempo de execucao e status da ordenacao
-	 //algoritmo.mostrar(array, n);
-	 printf("N: %d - T: - %f s - O: %c.\n",n, total, ordem);
-	 //printf("isOrdenado: %s\n", isOrdenado(array, n) ? "true" : "false");
-
-    //Desalocar o espaco de memoria do array
-    free(array);
-
+	//Desalocar o espaco de memoria do array
+	free(array);
 }
 
+int main(int argc, char **argv)
+{
+	for(int i=2000; i<=128000; i=i*2) {
+		teste(i, 'C');
+		teste(i, 'D');
+		teste(i, 'A');
+	}
 
-
-
-
-//=============================================================================
-
-
-
-
-int main(int argc, char **argv) {
-
-    for(int i=2000; i<=128000; i=i*2){
-
-       teste(i, 'C');
-       teste(i, 'D');
-       teste(i, 'A');
-    }
-    
-
-
-    return 0;
+	return 0;
 }
