fc5fc28edc5cb9c4cdde403181829c8ac6739396
Gustavo V. Castro <gustavovldcastro@gmail.com>
2021-09-27 17:16:01 -0300

    Fixed labels in wrong place

--- octave/octave_plot.m
+++ octave/octave_plot.m
@@ -29,31 +29,31 @@ data_counting_rand	= get_data_by_id(data, 8, 3);
 % Increassing
 plot_time_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
 % Decreassing
-%plot_time_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
+plot_time_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
 % Random
-%plot_time_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
+plot_time_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
 
 
 % Plotting size x time (scalling for fastest algorithms)
 % Increassing
-%plot_fastest_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
+plot_fastest_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
 % Decreassing
-%plot_fastest_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
+plot_fastest_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
 % Random
-%plot_fastest_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
+plot_fastest_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
 
 % Plotting size x memory
 % Increassing
-%plot_mem_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
+plot_mem_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
 % Decreassing
-%plot_mem_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
+plot_mem_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
 % Random
-%plot_mem_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
+plot_mem_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
 
 % Plotting size x memory (scanlling for lightest algorithms)
 % Increassing
-%plot_lightest_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
+plot_lightest_incr(data_bubble_incr, data_selection_incr, data_insertion_incr, data_merge_incr, data_quick_incr, data_shell_incr, data_heap_incr, data_counting_incr);
 % Decreassing
-%plot_lightest_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
+plot_lightest_decr(data_bubble_decr, data_selection_decr, data_insertion_decr, data_merge_decr, data_quick_decr, data_shell_decr, data_heap_decr, data_counting_decr);
 % Random
-%plot_lightest_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
+plot_lightest_rand(data_bubble_rand, data_selection_rand, data_insertion_rand, data_merge_rand, data_quick_rand, data_shell_rand, data_heap_rand, data_counting_rand);
